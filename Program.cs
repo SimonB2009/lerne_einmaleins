5b9ba125e05779284934d9e85f3b5c6652d9836f
SimonB2009 <brandstetter.simon@web.de>
2022-06-17 12:29:26 +0200

    code hinzugefuegt

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System; 
+
+
+
+namespace simon { 
+
+    
+    
+
+    class Konsolenraser
+    {
+
+        static void Main(string[] args) {
+
+            int a, b, erg;
+            char c = 'q';
+
+            Random r = new Random(); 
+
+            while (c != 'b') {
+
+                if (Console.KeyAvailable == true)
+                {
+                    c = Console.ReadKey(true).KeyChar;
+
+                }
+                
+
+                a = r.Next(0, 11);
+                b = r.Next(0, 11);
+                erg = a * b;
+
+                Console.Write("     " + a + " * " + b + " = ");
+
+                if (Console.ReadLine() == erg.ToString()) {
+
+                    Console.WriteLine("     Richtig!");
+                } else {
+
+                    Console.WriteLine("     Falsch!");
+                }
+            }
+        }
+    
+
+    }
+}
